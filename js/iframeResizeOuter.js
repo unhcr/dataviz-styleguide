6e3cc8313362167bc9b18e9197a775c7ea073743
curran <curran.kelleher@gmail.com>
2017-10-11 19:03:24 +0530

    Correct for border. Closes #90

--- js/iframeResizeOuter.js
+++ js/iframeResizeOuter.js
@@ -21,6 +21,11 @@
       // The data passed in the message is the height.
       var height = event.data;
 
+      // Compute the correction for the iframe border.
+      var border = parseFloat(getComputedStyle(iframe).borderWidth);
+      var borderCorrection = isNaN(border) ? 0 : border * 2;
+      height = height + borderCorrection;
+
       // Set the iframe height to the height from the message.
       iframe.setAttribute("height", height);
     }, false);
