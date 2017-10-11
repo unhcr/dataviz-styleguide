99a576ddc6211bd01678600a4aa82478f141dfb3
curran <curran.kelleher@gmail.com>
2017-10-11 17:58:23 +0530

    First pass at iframeResizeOuter

--- js/iframeResizeInner.js
+++ js/iframeResizeInner.js
@@ -1,21 +1,30 @@
 // This script is to be loaded at the end of the <body>
-// inside the page that will be embedded within another page
-// using an iframe.
+// inside the inner page that will be embedded
+// within another page using an iframe.
+//
 // This script will communicate the required height to the parent page,
 // so it can set the height of the iframe accordingly,
 // such that all the content is visible.
+// See https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
 (function (){
+
+  // This variable stores the last known height.
   var height = 0;
-  var targetOrigin = "*"; // See https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
 
   // If we are inside an iframe...
   if (window.parent) {
 
     // Check twice a second for resizes.
     setInterval(function () {
-      if (height !== window.innerWidth) {
-        height = window.innerWidth;
-        window.parent.postMessage(height, targetOrigin);
+
+      // If the height changed,
+      if (height !== window.innerHeight) {
+
+        // store the new height for future comparisons,
+        height = window.innerHeight;
+
+        // pass a message with the height to the parent window.
+        window.parent.postMessage(height, "*");
       }
     }, 500);
   }
