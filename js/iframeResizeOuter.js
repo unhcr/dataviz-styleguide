38a34776ac0707033779d6d70b3ab71b5173124e
curran <curran.kelleher@gmail.com>
2017-10-13 17:16:49 +0530

    Filter iframe messages

--- js/iframeResizeOuter.js
+++ js/iframeResizeOuter.js
@@ -11,23 +11,27 @@
   // This code assumes there is only a single iframe on the page.
   var iframe = document.querySelector("iframe");
 
+  // Sets the corrected height of the iframe.
+  function resizeIframe (height){
+
+    // Compute the correction for the iframe border.
+    var border = parseFloat(getComputedStyle(iframe).borderWidth);
+    var borderCorrection = isNaN(border) ? 0 : border * 2;
+    height = height + borderCorrection;
+
+    // Set the iframe height to the height from the message.
+    iframe.setAttribute("height", height);
+  }
+
   // If an iframe is there...
   if (iframe) {
 
     // Listen for messages from the inner iframe.
     // See https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
     window.addEventListener("message", function (event) {
-
-      // The data passed in the message is the height.
-      var height = event.data;
-
-      // Compute the correction for the iframe border.
-      var border = parseFloat(getComputedStyle(iframe).borderWidth);
-      var borderCorrection = isNaN(border) ? 0 : border * 2;
-      height = height + borderCorrection;
-
-      // Set the iframe height to the height from the message.
-      iframe.setAttribute("height", height);
+      if (event.data.type === 'iFrameResizeHeight') {
+        resizeIframe(event.data.height);
+      }
     }, false);
   }
 }());
